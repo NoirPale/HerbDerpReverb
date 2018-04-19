e53daacd4bf142c8a5edd5eddc6bb4472a6c0fc6
Unknown <Noirpale@gmail.com>
2018-04-19 10:49:03 +0200

    Dono some shit, heil. quote end

--- Knæp.c
+++ Knæp.c
@@ -4,7 +4,11 @@
 
 int main() {
   FILE * pFilein, *pFileout;
+  int Huff[22050] = { 0 };
   uint8_t buffer = 0;
+  uint16_t iter = 0;
+  float delay = 250, decay = 0.25;
+  int layS = (delay * 44.1f);
 
   pFilein = fopen("moeller.raw", "r");
   pFileout = fopen("bajer.raw", "w");
@@ -13,9 +17,16 @@ int main() {
     fputs("Error opening input file\n", stderr);
     exit(1);
   }
-
   while(feof(pFilein) == 0) {
     fread(&buffer, 1, 1, pFilein);
+    buffer += *(Huff + iter);
+        *(Huff + iter) = (decay * buffer);
+        iter++;
+        if (iter == (layS + 1))
+        {
+            printf("pølse.raw.\n");
+            iter = 0;
+        }
     fwrite(&buffer, 1, 1, pFileout);
   }
 
